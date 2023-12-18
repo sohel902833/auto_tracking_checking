ccf74da1356d737036111bce25f2f27d10fc13c5
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-12-18 23:48:46 +0600

    [775649]first changes

---
+++ hello.js
@@ -0,0 +1,38 @@
+let obj = {};
+let strt = 60;
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+
+console.log(obj);
+
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
