6bd0354984b03a65c02c026b9805677c08a5dc11
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-12-18 23:51:05 +0600

    [72261]second developer commit 1

--- hello.js
+++ hello.js
@@ -84,3 +84,11 @@ for (let i = 1; i <= 31; i++) {
   strt++;
   obj[i.toString()] = strt.toString();
 }
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
+for (let i = 1; i <= 31; i++) {
+  strt++;
+  obj[i.toString()] = strt.toString();
+}
